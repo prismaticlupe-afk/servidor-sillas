6f0d798113c5bdc7d52543c4891a8502fbe49eb6
prismaticlupe-afk <prismaticlupe@gmail.com>
2025-11-28 11:16:45 -0600

    Implement chat message handling in Socket.IO
    Added chat functionality to the Socket.IO server.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const http = require("http");
 const { Server } = require("socket.io");
 
@@ -6,7 +7,7 @@ const httpServer = http.createServer((req, res) => {
   res.end();
 });
 
-// ESTO ES LO QUE ARREGLA TU PROBLEMA (CORS)
+// Configuración de CORS
 const io = new Server(httpServer, {
   cors: {
     origin: "*", 
@@ -17,21 +18,31 @@ const io = new Server(httpServer, {
 io.on("connection", (socket) => {
   console.log("Jugador conectado:", socket.id);
 
+  // 1. Unirse a sala
   socket.on("JOIN_ROOM", ({ room, player }) => {
     socket.join(room);
     socket.to(room).emit("PLAYER_JOINED", player);
   });
 
+  // 2. Movimiento
   socket.on("UPDATE_POS", (data) => {
     socket.to(data.room).emit("UPDATE_POS", data);
   });
 
+  // 3. Eventos del juego (Sillas)
   socket.on("GAME_EVENT", (data) => {
     io.in(data.room).emit("GAME_EVENT", data);
   });
+
+  // 4. CHAT (¡ESTO ES LO NUEVO!)
+  socket.on("CHAT_MESSAGE", (data) => {
+    // data tiene: { room, name, text }
+    // io.in envía el mensaje a TODOS en la sala (incluido tú)
+    io.in(data.room).emit("CHAT_MESSAGE", data);
+  });
+
 });
 
-// Render usa el puerto que le da la gana, esto lo detecta auto
 const PORT = process.env.PORT || 3000;
 httpServer.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
